218b2b9cfa2ebeda21bee5cf0c6654926b82bb0d
Luis Felipe Nascimento Santana <93007295+LuisFelipeVTX9@users.noreply.github.com>
2022-07-30 18:32:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+// NAVBAR
+class MobileNavbar{
+	constructor(mobileMenu, navList, navLinks){
+		this.mobileMenu = document.querySelector(mobileMenu)
+		this.navList = document.querySelector(navList)
+		this.navLinks = document.querySelectorAll(navLinks)
+		this.activeClass = "active"
+
+		this.handleClick = this.handleClick.bind(this)
+	}
+
+	handleClick(){
+		this.navList.classList.toggle(this.activeClass)
+	}
+
+	addClickEvent(){
+		this.mobileMenu.addEventListener("click", this.handleClick)
+	}
+
+	init(){
+		if(this.mobileMenu){
+			this.addClickEvent()
+		}
+		return this
+	}
+}
+
+const mobileNavbar = new MobileNavbar(
+	".btn-mobile",
+	".nav",
+	".nav .menu .item",
+	".nav .menu .special-item"
+)
+
+mobileNavbar.init()
+
+// BANNER ANIMATION
